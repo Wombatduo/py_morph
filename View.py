a692c3e19d4453436d92b64f34d25a652e6131fb
Wombatduo <v-laburenko@yandex.ru>
2019-11-18 21:35:56 +0300

    added GET endpoint for EnglishVerb

--- View.py
+++ View.py
@@ -1,11 +1,14 @@
 #!venv/bin/python
-from flask import Flask
+from flask import Flask, jsonify
+
+from EnglishVerb import EnglishVerb
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, again, World!"
+@app.route('/morph/<infinitive>', methods=['GET'])
+def index(infinitive):
+    verb = EnglishVerb(infinitive)
+    return jsonify({'infinitive':verb.get_infinitive()})
 
 if __name__ == '__main__':
     app.run(debug=True)
