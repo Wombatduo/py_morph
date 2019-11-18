115680d95ffbd65b410273203a4649c0671bb1a3
halfahub <halfabit@gmail.com>
2019-11-18 22:13:06 +0300

    Revert "- POST http method added"
    This reverts commit 00053ff1

--- View.py
+++ View.py
@@ -10,7 +10,7 @@ def index(infinitive):
     verb = EnglishVerb(infinitive)
     return jsonify({'infinitive':verb.get_infinitive()})
 
-
+# curl -i -H "Content-Type: application/json" -X POST -d '{"person":"3","number":"1"}' http://127.0.0.1:5000/morph/be
 @app.route('/morph/<infinitive>', methods=['POST'])
 def morph_verb(infinitive):
     if not request.json or not 'person'  or not 'number' in request.json:
