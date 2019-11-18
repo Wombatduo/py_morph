f3c9dff272b2a592c96198d0266a974ba1570c96
halfahub <halfabit@gmail.com>
2019-11-18 22:11:15 +0300

    Test fixed

--- EnglishVerbTest.py
+++ EnglishVerbTest.py
@@ -5,14 +5,14 @@ from EnglishVerb import EnglishVerb, Person, Number
 class EnglishVerbTest(unittest.TestCase):
 
     def test_be_should_be_different_for_3_persons_singular(self):
-        self.assertEqual("am", EnglishVerb("be").morph(Person.FIRST, Number.SINGULAR, "Present", 'M'))
-        self.assertEqual("are", EnglishVerb("be").morph(Person.SECOND, Number.SINGULAR, "Present", 'M'))
-        self.assertEqual("is", EnglishVerb("be").morph(Person.THIRD, Number.SINGULAR, "Present", 'M'))
+        self.assertEqual("am", EnglishVerb("be").morph(1, 1, "Present", 'M'))
+        self.assertEqual("are", EnglishVerb("be").morph(2, 1, "Present", 'M'))
+        self.assertEqual("is", EnglishVerb("be").morph(3, 1, "Present", 'M'))
 
     def test_be_should_be_are_for_3_persons_plural(self):
-        self.assertEqual("are", EnglishVerb("be").morph(Person.FIRST, Number.PLURAL, "Present", 'M'))
-        self.assertEqual("are", EnglishVerb("be").morph(Person.SECOND, Number.PLURAL, "Present", 'M'))
-        self.assertEqual("are", EnglishVerb("be").morph(Person.THIRD, Number.PLURAL, "Present", 'M'))
+        self.assertEqual("are", EnglishVerb("be").morph(1, 2, "Present", 'M'))
+        self.assertEqual("are", EnglishVerb("be").morph(2, 2, "Present", 'M'))
+        self.assertEqual("are", EnglishVerb("be").morph(3, 2, "Present", 'M'))
 
     def test_get_infinitive(self):
         self.assertEqual(EnglishVerb("be").get_infinitive(), "be")
