7c79b413fde7b44cc0587434102790fc8630a064
Лабуренко Александр Витальевич <avlaburenko@sberbank.ru>
2020-05-10 14:50:23 +0300

    add Rus verb test case

--- RussianVerbTest.py
+++ RussianVerbTest.py
@@ -12,6 +12,16 @@ class RussianVerbTest(unittest.TestCase):
         self.assertEqual("делаешь", RussianVerb("делать").morph(2, 1, 2, 'M'))
         self.assertEqual("делает", RussianVerb("делать").morph(3, 1, 2, 'M'))
 
+    def test_should_be_different_for_3_persons_past_singular(self):
+        self.assertEqual("делал", RussianVerb("делать").morph(1, 1, 1, 'M'))
+        self.assertEqual("делал", RussianVerb("делать").morph(2, 1, 1, 'M'))
+        self.assertEqual("делал", RussianVerb("делать").morph(3, 1, 1, 'M'))
+
+    def test_should_be_different_for_3_persons_future_singular(self):
+        self.assertEqual("буду делать", RussianVerb("делать").morph(1, 1, 3, 'M'))
+        self.assertEqual("будешь делать", RussianVerb("делать").morph(2, 1, 3, 'M'))
+        self.assertEqual("будет делать", RussianVerb("делать").morph(3, 1, 3, 'M'))
+
 
 if __name__ == '__main__':
     unittest.main()
