b5c2355ff3cf544b724cae628da6a6133f1aee89
Лабуренко Александр Витальевич <avlaburenko@sberbank.ru>
2020-11-13 22:23:25 +0300

    dynamic test russian (finished)

--- langs/russian/RussianVerbTest_dyn.py
+++ langs/russian/RussianVerbTest_dyn.py
@@ -2,36 +2,55 @@ import csv
 import unittest
 from os import path, listdir
 
+from langs.russian.RussianVerb import RussianVerb
+
+
+# write tests
+# write code
+# refactor tests
+# refactor code
+
 
 class RussianVerbTest(unittest.TestCase):
 
+    def test_get_infinitive(self):
+        self.assertEqual("быть", RussianVerb("быть").get_infinitive())
+
     def test_get_pronouns(self):
+        self.assertEqual("я", RussianVerb.get_pronoun(1, 1, 1))
+        self.assertEqual("ты", RussianVerb.get_pronoun(2, 1, 1))
+        self.assertEqual("он", RussianVerb.get_pronoun(3, 1, 1))
+        self.assertEqual("она", RussianVerb.get_pronoun(3, 1, 2))
+        self.assertEqual("оно", RussianVerb.get_pronoun(3, 1, 3))
+        self.assertEqual("мы", RussianVerb.get_pronoun(1, 2, 1))
+        self.assertEqual("вы", RussianVerb.get_pronoun(2, 2, 1))
+        self.assertEqual("они", RussianVerb.get_pronoun(3, 2, 1))
+
+    def test_get_verb_forms(self):
         test_verbs_path = path.join('test_verbs')
         files = listdir(test_verbs_path)
-
         print(files)
         for file in files:
-            tense = 1
-            number = 2
+            infinitive = file[:file.index('.')]
             with open(path.join('test_verbs', file), newline='\n') as verb_forms:
                 forms_list = list(csv.reader(verb_forms, csv.unix_dialect, delimiter='\t'))
-                for forms in forms_list:
-                    for p, form in enumerate(forms):
-                        if p == 0:
-                            if number == 2:
-                                number = 1
-                            else:
-                                number = 2
-                        for e in range(1, 18):
-                            tense = tense + 1
-                            if tense <= 6:
-                                tense = 1
-                            elif 6 <= tense <= 12:
-                                tense = 2
-                            else:
-                                tense = 3
-                        p = p + 1
-                        print(tense, p, number, form)
+                for row, forms in enumerate(forms_list):
+                    number = (row & 1) + 1
+                    row += 1  # increment (i++ for C successors)
+                    # 1 // 2 == 0      1 & 1 == 1
+                    # 2 // 2 == 1      2 & 1 == 0
+                    # 3 // 2 == 1      3 & 1 == 1
+                    # 4 // 2 == 2      4 & 1 == 0
+                    # 5 // 2 == 2      5 & 1 == 1
+                    # 6 // 2 == 3      6 & 1 == 0
+                    tense = (row // 2) + (row & 1)
+                    #  8421
+                    #  1101 & 0001 == 0001
+                    #  1100 & 0001 == 0000
+                    for person, form in enumerate(forms):
+                        person += 1
+                        # print(tense, person, number, form)
+                        self.assertEqual(form, RussianVerb(infinitive).morph(person, number, tense, 'M'))
 
         if __name__ == '__main__':
             unittest.main()
